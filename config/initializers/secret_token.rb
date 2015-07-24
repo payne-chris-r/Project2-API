9a8a766c5ece9c5d0ee459ca45b820b88df3d561
payne-chris-r <06cpayne@gmail.com>
2015-07-24 10:04:13 -0400

    Just initialized new rails-api using postgreSQL
    This is just boilerplate, nothing fancy added yet.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Project2Api::Application.config.secret_token = '39113502ebef3ee0ebf27a3a8e97df53f2cbf5b0436019b6c92c54f7b53d9fb41024fe1f81fe973e537b665d5091cfc3b3c52c01ce19be9f6957136c82a7986f'
