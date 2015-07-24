f30780c6cb8606c9982d69880d31fe82f8a2d705
payne-chris-r <06cpayne@gmail.com>
2015-07-24 13:56:19 -0400

    Added a token. Removed and then added users to randomly generate a token on creation.
    Shit was blowin up 'cause we required a token and users that were already imported didn't have a token as a value, so removed then readded them to resolve that issue.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  before_create :set_token
+
   validates :email, uniqueness: true
+
+  #normally we want these to expire after x days here.
+  def set_token
+    self.token = SecureRandom.hex
+  end
 end
