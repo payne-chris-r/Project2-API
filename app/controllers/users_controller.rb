9beb9f8702cd84e1634242116b65d1f435db94fa
payne-chris-r <06cpayne@gmail.com>
2015-07-24 16:29:21 -0400

    Boom baby. Got the API working using our tictactoe FE from the JQuery/Ajax Lab

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :authenticate, only: [:login, :create]
+
+  #before_action :set_user, only: [:show, :update, :destroy]
+
+
+  #POST /login
+  def login
+    credentials = user_credentials
+    token = User.login(credentials[:email], credentials[:password])
+    if token
+      render json: { token: token }
+    else
+      head :unauthorized
+    end
+  end
 
   # GET /users
   # GET /users.json
@@ -18,7 +32,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_credentials)
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -48,12 +62,16 @@ class UsersController < ApplicationController
   end
 
   private
+  def user_credentials
+    params.require(:credentials).permit(:email,
+                                        :password,
+                                        :password_confirmation)
+  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  # def user_params
+  #   params.require(:user).permit(:email, :password, :password_confirmation)
+  # end
 end
