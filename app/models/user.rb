fbbe2f1088c76bf796864afe02ef51257cc9db5f
payne-chris-r <06cpayne@gmail.com>
2015-07-24 14:21:51 -0400

    Adds User.login and forces changing of token when we see a new login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: true
 
+  def self.login(email, password)
+    user = find_by email: email
+    user = user.authenticate password
+    user.set_token && user.save! if user
+    user.token if user
+  end
   #normally we want these to expire after x days here.
   def set_token
     self.token = SecureRandom.hex
