1a5da59c81cc1837f010f8495070f6b69cfaf0f4
payne-chris-r <06cpayne@gmail.com>
2015-08-07 12:55:30 -0400

    Furthest run works now.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,13 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :total_run_time, :total_run_distance, :average_pace, :token
+  attributes :id, :username, :total_run_time, :total_run_distance, :average_pace, :token, :furthest_run
 
-  def total_run_time
-    if object.runs.length != 0
-      object.runs.map{|run| run.time}.reduce(:+)
+  def furthest_run
+    ## return -1 if ....
+    if object.runs.length > 0
+      object.runs.map{|run| run.distance}.max
     else
       -1
-    end
-  end
+    end #if end
+  end #def end
 
   def total_run_distance
     if object.runs.length != 0
@@ -17,7 +18,63 @@ class UserSerializer < ActiveModel::Serializer
     end
   end
 
+  # def average_distance
+  #   if object.runs.length !=0
+
+  #   else
+  #     -1
+  #   end
+  # end
+
   def average_pace
     total_run_time/total_run_distance
   end
+
+  # def average_distance
+  #   if object.runs.length !=0
+
+  #   else
+  #     -1
+  #   end
+  # end
+
+  # def longest_run
+  #   if object.runs.length !=0
+
+  #   else
+  #     -1
+  #   end
+  # end
+
+  def total_run_time
+    if object.runs.length != 0
+      object.runs.map{|run| run.time}.reduce(:+)
+    else
+      -1
+    end
+  end
+
+  # def average_duration
+  #   if object.runs.length !=0
+
+  #   else
+  #     -1
+  #   end
+  # end
+
+  # def fastest_run
+  #   if object.runs.length !=0
+
+  #   else
+  #     -1
+  #   end
+  # end
+
+  # def average_pace
+  #   if object.runs.length !=0
+
+  #   else
+  #     -1
+  #   end
+  # end
 end
